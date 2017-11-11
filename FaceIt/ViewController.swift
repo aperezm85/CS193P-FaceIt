bce9bc88c33c3e076649fd0a47053cc37910e8a8
Alex Perez <aperezm85@gmail.com>
2017-11-11 15:27:51 +0000

    Assignment 2

--- FaceIt/ViewController.swift
+++ FaceIt/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            let handler = #selector(FaceView.changeScale(byReactingTo:))
+            let pinchRecognizer = UIPinchGestureRecognizer(target: faceView, action: handler)
+            faceView.addGestureRecognizer(pinchRecognizer)
+            
+            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo: )))
+            tapRecognizer.numberOfTapsRequired = 1
+            faceView.addGestureRecognizer(tapRecognizer)
+            
+            let swipeUpRecognizer = UISwipeGestureRecognizer(target:self, action: #selector(increaseHappiness))
+            swipeUpRecognizer.direction = .up
+            faceView.addGestureRecognizer(swipeUpRecognizer)
+            
+            let swipeDownRecognizer = UISwipeGestureRecognizer(target:self, action: #selector(decreaseHappiness))
+            swipeDownRecognizer.direction = .down
+            faceView.addGestureRecognizer(swipeDownRecognizer)
+            // execute the code every time the variable changes
+            updateUI()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func toggleEyes(byReactingTo tapRecognizer: UITapGestureRecognizer) {
+        if tapRecognizer.state == .ended {
+            let eyes: FacialExpression.Eyes = (expression.eyes == .closed) ? .open : .closed
+            expression = FacialExpression(eyes: eyes, mouth: expression.mouth)
+        }
     }
-
-
+    
+    // Swipe gesture is discrete, the gesture occurs or not
+    @objc func increaseHappiness() {
+        expression = expression.happier
+    }
+    
+    @objc func decreaseHappiness() {
+        expression = expression.sadder
+    }
+    
+    
+    var expression = FacialExpression(eyes: .closed, mouth:.frown) {
+        didSet {
+            // execute the code every time the variable changes
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true // we add the ? because we don't know if faceview exist at the begining of the program
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        faceView?.mouthCurvature = mouthCurvature[expression.mouth] ?? 0.0
+        
+    }
+    
+    // Dictionary for the mouth
+    private let mouthCurvature = [FacialExpression.Mouth.grin:0.5, .frown: -1.0, .smile:1.0, .neutral:0.0, .smirk: -0.5]
 }
 
